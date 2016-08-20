9d893fabdf12f064575e481e50a75ef51ec951ed
Alex Bahouth <ajbahouth@gmail.com>
2016-08-20 15:06:25 -0700

    Re-commit: Simplify background image management
    Updated Background Resource selection to simplify code and O(1) selection of new resource.
    
    * Preloaded all of the background image resources into an Array to ensure O(1) selection instead of If-then-else / switch selection
    * Typedef'd the Resource_ID so that if pebble changes from int32u_t later, don't need to change all the code.
    * There was a lot of random number manipulation, that, itself seemed random. Simplified and cleaned up that code.

--- src/perlin.c
+++ src/perlin.c
@@ -51,78 +51,71 @@ int charge_percent = 0;
 
 TextLayer *battery_text_layer;
 
-static int s_random = 20;
-static int temp_random;
+
+
+
+/**************** Background Images ****************/
+
+#define RESOURCE_COUNT 19
+typedef uint32_t RESOURCE_ID;
+
+static RESOURCE_ID background_image_resource_ids[RESOURCE_COUNT] = {
+  RESOURCE_ID_IMAGE_BG1,
+  RESOURCE_ID_IMAGE_BG2,
+  RESOURCE_ID_IMAGE_BG3,
+  RESOURCE_ID_IMAGE_BG4,
+  RESOURCE_ID_IMAGE_BG5,
+  RESOURCE_ID_IMAGE_BG6,
+  RESOURCE_ID_IMAGE_BG7,
+  RESOURCE_ID_IMAGE_BG8,
+  RESOURCE_ID_IMAGE_BG9,
+  RESOURCE_ID_IMAGE_BG10,
+  RESOURCE_ID_IMAGE_BG11,
+  RESOURCE_ID_IMAGE_BG12,
+  RESOURCE_ID_IMAGE_BG13,
+  RESOURCE_ID_IMAGE_BG14,
+  RESOURCE_ID_IMAGE_BG15,
+  RESOURCE_ID_IMAGE_BG16,
+  RESOURCE_ID_IMAGE_BG17,
+  RESOURCE_ID_IMAGE_BG18,
+  RESOURCE_ID_IMAGE_BG19,
+};
+
+RESOURCE_ID choose_background_image_resource_id(int key) {
+  // saftey first!
+  return background_image_resource_ids[key%RESOURCE_COUNT]; 
+}
+
+GBitmap * select_new_background_image() {
+  int random_selection;
+  RESOURCE_ID r_id;
+  GBitmap * new_bg_image;
+
+  random_selection = rand();
+  r_id = choose_background_image_resource_id(random_selection);
+  new_bg_image = gbitmap_create_with_resource(r_id);
+  return new_bg_image;    
+}
 
 
 void theme_choice() {	
-		
-			if(s_random == 19){
-			s_random = 0;
-		} else {
-
-			temp_random = rand() % 19;
-
-			while(temp_random == s_random){
-			    temp_random = rand() % 19;
-		    }
-
-		    s_random = temp_random;
-
-	    if (background_image) {
-		gbitmap_destroy(background_image);
-		background_image = NULL;
-    }
-
-		   if(s_random == 0){
-			   background_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG1);
-         } else if(s_random == 1){
-				background_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG2);
-         } else if(s_random == 2){
-				background_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG3);
-         } else if(s_random == 3){
-				background_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG4);
-         } else if(s_random == 4){
-				background_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG5);
-         } else if(s_random == 5){
-				background_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG6);
-         } else if(s_random == 6){
- 				background_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG7);
-         } else if(s_random == 7){
- 				background_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG8);
-         } else if(s_random == 8){
- 				background_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG9);
-         } else if(s_random == 9){
- 				background_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG10);
-         } else if(s_random == 10){
- 				background_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG11);
-         } else if(s_random == 11){
- 				background_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG12);
-         } else if(s_random == 12){
- 				background_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG13);
-         } else if(s_random == 13){
- 				background_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG14);
-         } else if(s_random == 14){
- 				background_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG15);
-         } else if(s_random == 15){
- 				background_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG16);
-         } else if(s_random == 16){
- 				background_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG17);
-         } else if(s_random == 17){
- 				background_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG18);
-         } else if(s_random == 18){
- 				background_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG19);
-         } else if(s_random == 19){
- 				background_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG20);
-		 }
-
-	   if (background_image != NULL) {
+    
+  // collect previous image
+  if (background_image) {
+    gbitmap_destroy(background_image);
+  	background_image = NULL;
+  }
+  // set new image
+  background_image = select_new_background_image();
+      
+  if (background_image != NULL) {
 		bitmap_layer_set_bitmap(background_layer, background_image);
 		layer_set_hidden(bitmap_layer_get_layer(background_layer), false);
 		layer_mark_dirty(bitmap_layer_get_layer(background_layer));
-    }		
   }
+  // TODO log warning here if Null is selected
 }
+
 
 static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tuple, const Tuple* old_tuple, void* context) {
   switch (key) {
